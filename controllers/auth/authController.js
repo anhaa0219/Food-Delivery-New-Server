b5a48cc8029c94cb9123748c054397a820b48f85
anhaa0219 <anhaadiamond2002@gmail.com>
2026-09-14 20:44:36 +0800

    ochigdriin hashpassword heseg hiisen

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { User } from "../../schemas/user-schema.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 const SALT_ROUND = 10;
+const JWT_SECRET = "testing"
+const signAuthToken = (user)=> {
+  return jwt.sign({email : user.email , password : user.password},JWT_SECRET,{expiresIn : "1d"})
+}
 export const SignUpController = async (request, response) => {
   try {
     const { email, password } = request.body;
@@ -11,7 +16,8 @@ export const SignUpController = async (request, response) => {
       message1: "user created ",
       user: user,
     });
-    response.status(201).json({ message: "User Created", user: user });
+    const token = signAuthToken(user)
+    response.status(201).json({ message: "User Created", user: user, token : token });
   } catch (err) {
     response.status(500).json({ message: "Internal Server Error", error: err });
   }
@@ -27,7 +33,8 @@ export const LoginController = async (request, response) => {
     if (!comparedPassword) {
       return response.status(401).json({ message: "Wrong password" });
     }
-    return response.status(200).json({ message: "user found", user: user });
+    const token = signAuthToken(user)
+    return response.status(200).json({ message: "user found", user: user, token : token });
   } catch (err) {
     return response
       .status(500)
